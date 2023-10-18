18c4c9158bc5208e4e6b96edd98fca6ea5fddf32
Krish-Sy <krishnasurya974@gmail.com>
2023-10-18 17:50:59 +0530

    first commit

---
+++ models/Cart.js
@@ -0,0 +1,25 @@
+const CartSchema = new Schema({
+    userId: {
+        type: String,
+    },
+    items: [{
+        productId: {
+            type: String,
+        },
+        name: String,
+        quantity: {
+            type: Number,
+            required: true,
+            min: [1, 'Quantity can not be less then 1.'],
+            deafult: 1
+        },
+        price: Number
+    }],
+    bill: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+});
+
+module.exports = Cart = mongoose.model('cart',CartSchema);
